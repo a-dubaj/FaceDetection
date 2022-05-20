3f6387e38e5872d63a349b1621a3f4a835402733
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-05-20 15:04:28 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <img src="" alt="" width="940" height="650" />
+      <canvas width="940" height="650" />
     </div>
   );
 }
