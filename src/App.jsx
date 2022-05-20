75e3bafe73a55b4adbded83dc7e34b2011481a4b
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-05-20 16:55:47 +0200

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,75 @@
-import './App.css';
-import {useEffect, useRef} from "react";
-import * as faceapi from 'face-api.js';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import NewPost from "./components/NewPost";
 
 function App() {
-    const imgRef = useRef();
-    const canvasRef = useRef();
+    const [file, setFile] = useState();
+    const [image, setImage] = useState();
 
-    const handleImage = async () => {
-        const detection = await faceapi.detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-        console.log(detection);
-    }
     useEffect(() => {
-        const loadModels = () => {
-            Promise.all([
-                faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-                faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-                faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-                faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-            ]).then(handleImage).catch((e) => console.log(e));
+        const getImage = () => {
+            const img = new Image();
+            img.src = URL.createObjectURL(file);
+            img.onload = () => {
+                setImage({
+                    url: img.src,
+                    width: img.width,
+                    height: img.height,
+                });
+            };
         };
-        imgRef.current && loadModels();
-    }, []);
+
+        file && getImage();
+    }, [file]);
 
     return (
-        <div className="App">
-            <img
-                crossOrigin="anonymous"
-                ref={imgRef}
-                src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                alt="" width="940" height="650"/>
-            <canvas ref={canvasRef} width="940" height="650"/>
+        <div>
+            <Navbar />
+            {image ? (
+                <NewPost image={image} />
+            ) : (
+                <div className="newPostCard">
+                    <div className="addPost">
+                        <img
+                            src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                            alt=""
+                            className="avatar"
+                        />
+                        <div className="postForm">
+                            <input
+                                type="text"
+                                placeholder="What's on your mind?"
+                                className="postInput"
+                            />
+                            <label htmlFor="file">
+                                <img
+                                    className="addImg"
+                                    src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                                    alt=""
+                                />
+                                <img
+                                    className="addImg"
+                                    src="https://icon-library.com/images/maps-icon-png/maps-icon-png-5.jpg"
+                                    alt=""
+                                />
+                                <img
+                                    className="addImg"
+                                    src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
+                                    alt=""
+                                />
+                                <button>Send</button>
+                            </label>
+                            <input
+                                onChange={(e) => setFile(e.target.files[0])}
+                                id="file"
+                                style={{ display: "none" }}
+                                type="file"
+                            />
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <span>Face App</span>
+        </div>
+    )
+}
+
+export default Navbar
